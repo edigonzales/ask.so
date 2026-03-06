04996c38707feb0e13cccff66efad88c95990e34
edigonzales <edi.gonzales@gmail.com>
2026-03-06 10:46:39 +0100

    initial commit

---
+++ src/main/java/ch/so/agi/AskSoApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskSoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskSoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/so/agi/AskSoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskSoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
